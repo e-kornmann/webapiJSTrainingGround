21f600caf96be5f7ffa9489ea18263999dba2dae
e-kornmann <elton.kornmann@gmail.com>
2023-01-08 03:47:10 +0100

    Add a test environment

---
+++ myapp/api.js
@@ -0,0 +1,147 @@
+const express = require('express');
+const app = express();
+
+
+app.use(express.json());
+const { check, validationResult } = require('express-validator');
+
+const db = [
+  {
+    id: 1,
+    name: 'Marcus Dev',
+    email: 'marcus@salt.dev'
+  },
+  {
+    id: 2,
+    name: 'Elton Kornmann',
+    email: 'elton.kornmann@appliedtechnology.se'
+  },
+];
+
+let nextId = db[db.length-1].id;
+let indexCounter = 0;
+
+
+
+// get redirected if someone requests homepage
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/static/simple-fetch-setup.html');
+});
+
+// GET DB OBJECT  
+app.get('/api/developers/', (req, res) => {
+res
+  .status(200)
+  .json(db);
+});
+
+
+// POST
+app.post('/api/developers/', [
+    check('name').escape().isString(),
+    check('email').isEmail()],
+    
+(req, res) => {
+  
+  //error handler
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+  
+  
+    //put in in the database
+    
+    const newDev = {
+      id: ++nextId,
+      name: req.body.name, 
+      email: req.body.email,
+    }
+
+    db.push(newDev);
+
+      
+    res
+     .status(201)
+     .setHeader('location', `/api/developers/${newDev.id}`)
+     .set('Accept', 'application/json')
+     .json(newDev);
+});
+      
+
+// GET
+app.get('/api/developers/:id', (req, res) => {
+  
+  const reqDev = db.find(i => i.id == req.params.id);
+  return reqDev ? 
+  // match
+    res
+    .status(200)
+    .setHeader('Location', `/api/developers/${req.params.id}`)   
+    .json(reqDev) :    
+  // no match
+        res
+        .status(404).end('Developer not found')
+     });
+
+
+// DELETE
+app.delete('/api/developers/:id', (req, res) => {
+        
+        const dev = db.find(i => i.id == req.params.id);
+
+        if (!dev) {
+          res.status(404).end('Developer not found');
+          return;
+        }
+       
+        
+        const index = db.indexOf(dev);
+        db.splice(index, 1);
+        --indexCounter;
+        // rearrange indexnr
+        
+        res
+        .status(204)
+        .setHeader('location', `/api/developers/${dev.id}`)   
+        .json(dev)        
+             
+      });
+   
+// PATCH
+app.patch('/api/developers/:id',[
+  check('name').escape().isString(),
+  check('email').isEmail()],
+     (req, res) => {
+      //find id
+      const devId = parseInt(req.params.id);
+      const dev = db.find(i => i.id == devId);
+        
+      if (!dev) {
+        res.status(404).send('Developer not found');
+        return;
+      }
+
+      
+      if (parseInt(req.body.id) !== parseInt(req.params.id)) {  
+        res.status(400).send('Cannot update your user ID, please try again')
+        return;
+     }
+
+
+      dev.name = JSON.stringify(req.body.name);
+      dev.email = JSON.stringify(req.body.email);
+      res
+      .status(201)
+      .setHeader('location', `/api/developers/${dev.id}`)
+      .json(dev);
+    
+    });
+
+
+    module.exports = {
+        app,
+        db,
+        nextId,
+        indexCounter,
+      }
--- myapp/index.js
+++ myapp/index.js
@@ -1,131 +1,20 @@
-const express = require('express');
-const dataServer = express();
-
-dataServer.use(express.json());
-const { check, validationResult } = require('express-validator');
-
-const db = [
-  {
-    id: 1,
-    name: 'Marcus Dev',
-    email: 'marcus@salt.dev'
-  },
-  {
-    id: 2,
-    name: 'Elton Kornmann',
-    email: 'elton.kornmann@appliedtechnology.se'
-  }
-];
-
-
-
-
-// get redirected if someone requests homepage
-dataServer.get('/', function(req, res) {
-  res.sendFile(__dirname + '/static/simple-fetch-setup.html');
-});
-
-// get db object with all devs 
-dataServer.get('/api/developers/', (req, res) => {
-res
-  .status(200)
-  .json(db);
-});
-
-
-// POST
-dataServer.post('/api/developers/', 
-    check('email').isEmail(),
-
-
-(req, res) => {
-  
-  //error handler
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(400).json({ errors: errors.array() });
-  }
-  
-  
-    //put in in the database
-    const newDev = {
-      id: db.length + 1,
-      name: req.body.name,
-      email: req.body.email,
-    }
-
-    db.push(newDev);
-      
-    res
-     .status(201)
-     .setHeader('location', `/api/developers/${newDev.id}`)
-     .json(newDev);
-});
-      
-
-// GET
-dataServer.get('/api/developers/:id', (req, res) => {
-  
-  const reqDev = db.find(i => i.id == req.params.id);
-  return reqDev ? 
-  // match
-    res
-    .status(200)
-    .setHeader('location', `/api/developers/${req.params.id}`)   
-    .json(reqDev) :    
-  // no match
-        res
-        .status(404).end('Developer not found')
-     });
-
-
-// DELETE
-dataServer.delete('/api/developers/:id', (req, res) => {
-  
-        const dev = db.find(i => i.id == req.params.id);
-
-        if (!dev) {
-          res.status(404).end('Developer not found');
-          return;
-        }
-       
-        const index = db.indexOf(dev);
-        db.splice(index, 1);
-        res
-        .status(204)
-        .setHeader('location', `/api/developers/${dev.id}`)   
-        .json(dev)        
-             
-      });
-   
-// PATCH
-dataServer.patch('/api/developers/:id', (req, res) => {
-
-      const devId = parseInt(req.params.id);
-      const dev = db.find(i => i.id == devId);
-        
-      if (!dev) {
-        res.status(404).send('Developer not found');
-        return;
-      }
-
-      if (req.body.id !== devId) {
-         res.status(400).send('Cannot update your user ID, please try again')
-         return;
-      }
-
-      dev.name = req.body.name;
-      dev.email = req.body.email;
-      res
-      .status(201)
-      .setHeader('location', `/api/developers/${dev.id}`)
-      .json(dev);
-    
-    });
-
+const { app } = require('./api.js');
+const { db } = require('./api.js');
 
 const port = 3000
 
-dataServer.listen(port, () => {
+app.listen(port, () => {
+      
+});
+
+/*
+
+alternatively
+const api = require('./api.js');
+
+const port = 3000   
+
+api.app.listen(port, () => {
     console.log('Lekker bezig El.')
 });
+*/
