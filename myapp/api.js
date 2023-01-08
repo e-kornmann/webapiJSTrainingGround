e69e2ff0f0a17f72cc5b9bc893f8dccd7ce11bde
e-kornmann <elton.kornmann@gmail.com>
2023-01-08 13:55:32 +0100

    Completed the test environment

--- myapp/api.js
+++ myapp/api.js
@@ -18,130 +18,133 @@ const db = [
   },
 ];
 
-let nextId = db[db.length-1].id;
+let nextId = db[db.length - 1].id;
 let indexCounter = 0;
 
 
 
 // get redirected if someone requests homepage
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   res.sendFile(__dirname + '/static/simple-fetch-setup.html');
 });
 
 // GET DB OBJECT  
 app.get('/api/developers/', (req, res) => {
-res
-  .status(200)
-  .json(db);
+  res
+    .status(200)
+    .json(db);
 });
 
 
 // POST
 app.post('/api/developers/', [
-    check('name').escape().isString(),
-    check('email').isEmail()],
-    
-(req, res) => {
-  
-  //error handler
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(400).json({ errors: errors.array() });
-  }
-  
-  
+  check('name').escape().isString(),
+  check('email').isEmail()],
+
+  (req, res) => {
+
+    //error handler
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+
     //put in in the database
-    
+
     const newDev = {
       id: ++nextId,
-      name: req.body.name, 
+      name: req.body.name,
       email: req.body.email,
     }
 
     db.push(newDev);
 
-      
+
     res
-     .status(201)
-     .setHeader('location', `/api/developers/${newDev.id}`)
-     .set('Accept', 'application/json')
-     .json(newDev);
-});
-      
+      .status(201)
+      .setHeader('location', `/api/developers/${newDev.id}`)
+      .set('Accept', 'application/json')
+      .json(newDev);
+  });
+
 
 // GET
 app.get('/api/developers/:id', (req, res) => {
-  
+
   const reqDev = db.find(i => i.id == req.params.id);
-  return reqDev ? 
-  // match
+  return reqDev ?
+    // match
     res
-    .status(200)
-    .setHeader('Location', `/api/developers/${req.params.id}`)   
-    .json(reqDev) :    
-  // no match
-        res
-        .status(404).end('Developer not found')
-     });
+      .status(200)
+      .setHeader('Location', `/api/developers/${req.params.id}`)
+      .json(reqDev) :
+    // no match
+    res
+      .status(404).end('Developer not found')
+});
 
 
 // DELETE
 app.delete('/api/developers/:id', (req, res) => {
-        
-        const dev = db.find(i => i.id == req.params.id);
 
-        if (!dev) {
-          res.status(404).end('Developer not found');
-          return;
-        }
-       
-        
-        const index = db.indexOf(dev);
-        db.splice(index, 1);
-        --indexCounter;
-        // rearrange indexnr
-        
-        res
-        .status(204)
-        .setHeader('location', `/api/developers/${dev.id}`)   
-        .json(dev)        
-             
-      });
-   
+  const dev = db.find(i => i.id == req.params.id);
+
+  if (!dev) {
+    res.status(404).end('Developer not found');
+    return;
+  }
+
+
+  const index = db.indexOf(dev);
+  db.splice(index, 1);
+  --indexCounter;
+  // rearrange indexnr
+
+  res
+    .status(204)
+    .setHeader('location', `/api/developers/${dev.id}`)
+    .json(dev)
+
+});
+
 // PATCH
-app.patch('/api/developers/:id',[
+app.patch('/api/developers/:id', [
   check('name').escape().isString(),
   check('email').isEmail()],
-     (req, res) => {
-      //find id
-      const devId = parseInt(req.params.id);
-      const dev = db.find(i => i.id == devId);
-        
-      if (!dev) {
-        res.status(404).send('Developer not found');
-        return;
-      }
+  (req, res) => {
+    //find id
+    const devId = parseInt(req.params.id);
+    const dev = db.find(i => i.id == devId);
 
-      
-      if (parseInt(req.body.id) !== parseInt(req.params.id)) {  
+    if (!dev) {
+      res.status(404).send('Developer not found');
+      return;
+    }
+
+
+    if (req.body.id) {
+      if (parseInt(req.body.id) !== parseInt(req.params.id)) {
         res.status(400).send('Cannot update your user ID, please try again')
         return;
-     }
+      }
+    }
 
 
-      dev.name = JSON.stringify(req.body.name);
-      dev.email = JSON.stringify(req.body.email);
-      res
+    dev.name = req.body.name;
+    dev.email = req.body.email;
+    res
       .status(201)
       .setHeader('location', `/api/developers/${dev.id}`)
+      .set('Content-Type', 'application/json')
       .json(dev);
-    
-    });
+
+  });
 
 
-    module.exports = {
-        app,
-        db,
-        nextId,
-        indexCounter,
-      }
+module.exports = {
+  app,
+  db,
+  nextId,
+  indexCounter,
+} 
--- myapp/mochatest.js
+++ myapp/mochatest.js
@@ -2,14 +2,14 @@ const assert = require('assert');
 const request = require('supertest');
 const { app, db } = require('./api.js');
 
-let nextId = db[db.length-1].id;
+let nextId = db[db.length - 1].id;
 let indexCounter = db.length - 1;
 
 //=1
 describe('1. developer API should have endpoints to', () => {
 
-  it('get all developers', function(done) {
-     // act and assert
+  it('get all developers', function (done) {
+    // act and assert
     request(app)
       .get('/api/developers')
       .set('Accept', 'application/json')
@@ -19,10 +19,10 @@ describe('1. developer API should have endpoints to', () => {
       })
       .expect(200, done);
   });
-//=2
-  it('get the last developers by ID request', function(done) {
+  //=2
+  it('get the last developers by ID request', function (done) {
     // arrange
-    
+
     // act and assert
     request(app)
       .get(`/api/developers/${nextId}`)
@@ -30,98 +30,68 @@ describe('1. developer API should have endpoints to', () => {
       .expect('Content-Type', /json/)
       .expect('location', `/api/developers/${nextId}`)
       .expect((res) => {
-        assert.strictEqual(JSON.stringify(res.body), JSON.stringify(db[indexCounter]))})  
+        assert.strictEqual(JSON.stringify(res.body), JSON.stringify(db[indexCounter]))
+      })
       .expect(200, done);
   });
-//=3
-  it('get a succesfull develeper subscrition', function(done) {
+  //=3
+  it('get a succesfull develeper subscrition', function (done) {
     // arrange
     let id = ++nextId;
-    
+
     // act and assert
     request(app)
       .post('/api/developers/')
-      .send({"name":"Beatrice Dev","email":"bea@salt.dev"})
+      .send({ "name": "Beatrice Dev", "email": "bea@salt.dev" })
       .set('Accept', 'application/json')
       .expect('Content-Type', /json/)
       .expect('location', `/api/developers/${id}`)
       .expect((res) => {
-        assert.strictEqual(JSON.stringify(res.body), JSON.stringify(db[++indexCounter]))})  
-        .expect((res) => {
-          assert.strictEqual(res.body.name, 'Beatrice Dev')})          
+        assert.strictEqual(JSON.stringify(res.body), JSON.stringify(db[++indexCounter]))
+      })
+      .expect((res) => {
+        assert.strictEqual(res.body.name, 'Beatrice Dev')
+      })
       .expect(201, done);
   });
   //=4
-  it('deletete a misfunctional developer', function(done) {
+  it('deletete a misfunctional developer', function (done) {
     //curl -X DELETE http://localhost:3000/api/developers/2 -i
     // arrange
     const id = 2;
-   
+
     // act and assert
     request(app)
       .delete(`/api/developers/${id}`)
       .expect('location', `/api/developers/${id}`)
       .expect((res) => {
-        assert.strictEqual(JSON.stringify(res.body), JSON.stringify({}))})  
-      .expect(204, done);   
+        assert.strictEqual(JSON.stringify(res.body), JSON.stringify({}))
+      })
+      .expect(204, done);
   });
-//=5*/
-  it('update a succesfull develeper subscrition', function(done) {
+  //=5
+  it('update a succesfull develeper subscrition', function (done) {
     // arrange
- 
-    const data = { 
+    const id = 1;
+
+    const data = {
       name: "Beatrice Dev",
       email: "bea@salt.dev",
     }
     // act and assert
     request(app)
-     .patch(`http://localhost:3000/api/developers/1`)
-     .send(data)
-     .set('Accept', 'application/json')
-      
-   //.patch('/api/developers/1')
-  // .send({ name: 'John', email: 'john@example.com' })
-  //  .expect('location', `/api/developers/${id}`)
-  //  .expect('Content-Type', /json/)
-    .expect((res) => {
-      assert.strictEqual(data.name, 'Beatrice Dev')})
-    .expect(200, done);
-  });
- });
-/*
-    .patch('/api/developers/2')
-    //.set('Accept', 'application/json')
-    .send({name: 'John', email: 'john@salt.dev'})
-    //.expect('Content-Type', /json/)
-  // .expect('location', '/api/developers/2')
-    .expect((res) => {
-      assert.strictEqual(app.name, "John");
-    })
-    .expect(201, done);
-  
-  });
-});
-
-    /*
-    // act and assert
-    request(app)
-      .patch(`/api/developers/:id`)
+      .patch(`/api/developers/${id}`)
+      .send(data)
+      .set('Asset', 'application/json')
+      .expect('Content-Type', /json/)
+      //.patch('/api/developers/1')
       .expect('location', `/api/developers/${id}`)
-      .send({"name":"Beatrice Dev","email":"bea@salt.dev"})
       .expect((res) => {
-    //    assert.strictEqual(res.body),(db[1])})  
-     //   .expect((res) => {
-         assert.strictEqual(res.body.name, 'Beatrice Torenstra')})          
+        assert.strictEqual(res.body.name, 'Beatrice Dev')
+      })
       .expect(201, done);
   });
-
 });
-
-
-  /*
-});
-
-
 
 
 
